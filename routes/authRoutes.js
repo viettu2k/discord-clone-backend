c784139b962bf58822c426918b597e672d8a3f2a
viettu2k <doanviettu2k@gmail.com>
2022-02-19 09:11:23 +0700

    updating login schema, register schema

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,14 +5,14 @@ const Joi = require("joi");
 const validator = require("express-joi-validation").createValidator({});
 
 const registerSchema = Joi.object({
-    username: Joi.string().min(3).max(12),
-    password: Joi.string().min(6).max(12),
-    mail: Joi.string().email(),
+    username: Joi.string().min(3).max(12).required(),
+    password: Joi.string().min(6).max(12).required(),
+    mail: Joi.string().email().required(),
 });
 
 const loginSchema = Joi.object({
-    password: Joi.string().min(6).max(12),
-    mail: Joi.string().email(),
+    password: Joi.string().min(6).max(12).required(),
+    mail: Joi.string().email().required(),
 });
 
 router.post(
