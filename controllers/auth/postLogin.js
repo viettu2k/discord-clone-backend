25ca38adb45165cbfbafe4217a23ce21f2c3aabe
viettu2k <doanviettu2k@gmail.com>
2022-02-19 10:41:03 +0700

    creating login functionality

--- controllers/auth/postLogin.js
+++ controllers/auth/postLogin.js
@@ -1,4 +1,25 @@
+const User = require("../../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const postLogin = async(req, res) => {
-    res.send("login router");
+    try {
+        const { mail, password } = req.body;
+
+        const user = await User.findOne({ mail: mail.toLowerCase() });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            // send new token
+            const token = "JWT TOKEN";
+
+            return res.status(200).json({
+                userDetails: { mail: user.mail, token: token, username: user.username },
+            });
+        }
+
+        return res.status(400).send("Invalid credentials, Please try again");
+    } catch (err) {
+        return res.status(500).send("Something went wrong. Please try again");
+    }
 };
 module.exports = postLogin;
