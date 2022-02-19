9138020b93a8ce4add2851460d917e38dcbc21d7
viettu2k <doanviettu2k@gmail.com>
2022-02-19 10:26:08 +0700

    creating register functionality

--- controllers/auth/postRegister.js
+++ controllers/auth/postRegister.js
@@ -1,4 +1,35 @@
+const User = require("../../models/user");
+const bcrypt = require("bcryptjs");
+
 const postRegister = async(req, res) => {
-    res.send("register router");
+    try {
+        const { username, mail, password } = req.body;
+
+        // check if user exists
+        const userExits = await User.exists({ mail: mail.toLowerCase() });
+
+        if (userExits) {
+            return res.status(409).send("E-mail already in use.");
+        }
+
+        // encrypt password
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        // create user document and save in database
+        const user = await User.create({
+            username,
+            mail: mail.toLowerCase(),
+            password: encryptedPassword,
+        });
+
+        // create JWT token
+        const token = "JWT TOKEN";
+
+        res.status(201).json({
+            userDetails: { mail: user.mail, token: token, user: user.name },
+        });
+    } catch (err) {
+        return res.status(500).send("Error occurred. Please try again");
+    }
 };
 module.exports = postRegister;
