e6eaa51c029486eb57b862e7d42f8f29ea79b0e8
viettu2k <doanviettu2k@gmail.com>
2022-06-07 09:29:44 +0700

    custom friend invitation validation

--- controllers/friendInvitation/postInvite.js
+++ controllers/friendInvitation/postInvite.js
@@ -1,5 +1,28 @@
+const User = require('../../models/user');
+
 const postInvite = async(req, res) => {
     const { targetMailAddress } = req.body;
+
+    const { userId, mail } = req.user;
+
+    // check if friend that we would like to invite is not user
+    if (mail.toLowerCase() === targetMailAddress.toLowerCase()) {
+        return res
+            .status(409)
+            .send('Sorry. You cannot become friend with yourself.');
+    }
+
+    const targetUser = await User.findOne({ mail: targetMailAddress });
+
+    if (!targetUser) {
+        return res
+            .status(404)
+            .send(
+                `Friend of ${targetMailAddress} has not been found. Please check mail address.`
+            );
+    }
+
+    // check if invitation has been already found
 
     return res.send('request passed');
 };
